bedd78c1e2bed31eea91feeffab345d20436840d
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 13:52:11 +0100

    Update proxmox-no-subscription.bash

--- proxmox-no-subscription.bash
+++ proxmox-no-subscription.bash
@@ -3,13 +3,10 @@ mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enter
 mv /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.bak
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
 
-cat <<EOL | tee /etc/apt/source.list
+cat <<EOL | tee /etc/apt/sources.list
 deb http://ftp.debian.org/debian bookworm main contrib
-deb http://ftp.debian.org/debian bookworm-updates main contrib
 
-# Proxmox VE pve-no-subscription repository provided by proxmox.com,
-# NOT recommended for production use
-deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription
+deb http://ftp.debian.org/debian bookworm-updates main contrib
 
 # security updates
 deb http://security.debian.org/debian-security bookworm-security main contrib
